9e242d8cc3d2a8856c0a26be18c8f696da0cf86d
priti <preity.bankar@gmail.com>
2012-09-24 13:51:12 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gupshup::Application.config.secret_token = '778ef56b7b72588c0f670317ee7a824bb66bb94277598a432ec0bb8009762f99eb83f806c2752e4ab48e9bf0bb65f5b57b1baa512737a4eb80952ae50699fbb2'
