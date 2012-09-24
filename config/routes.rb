bfa1189ca15877a894aa7547a86e103fd175ab4e
priti <preity.bankar@gmail.com>
2012-09-24 17:26:47 +0530

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Gupshup::Application.routes.draw do
 
   #get "static_pages/home"
   #match '/', to: 'static_pages#home'
-  root to: 'static_pages#home'
+  #root to: 'static_pages#home'
+  root :to => 'static_pages#home'
   # get "static_pages/help"
   match '/help', to: 'static_pages#help'
   # get "static_pages/about"
