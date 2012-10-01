711682af5005d0417732a5d903525ae4341f2fee
priti <preity.bankar@gmail.com>
2012-10-01 15:49:03 +0530

    change configuration for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Gupshup::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "http://gappagoshti.herokuapp.com" }
   config.perform_deliveries = true
   
   config.action_mailer.smtp_settings = {
@@ -79,6 +79,6 @@ Gupshup::Application.configure do
   }
 
   config.action_mailer.default_url_options = {
-    :host => "http://localhost:3000"
+    :host => "http://gappagoshti.herokuapp.com"
   }
 end
