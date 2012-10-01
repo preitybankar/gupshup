ad3320970be9e53e431985dd986db3b880e89f9a
priti <preity.bankar@gmail.com>
2012-10-01 16:09:39 +0530

    change configuration for smtp

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,6 @@
 class PasswordResetsController < ApplicationController
   def new
   end
-  
-  
 
  def create
   user = User.find_by_email(params[:email])
@@ -10,22 +8,17 @@ class PasswordResetsController < ApplicationController
   redirect_to root_url, :notice => "Email sent with password reset instructions."
  end
 
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
+  end
   
-
-def edit
-  @user = User.find_by_password_reset_token!(params[:id])
-end
-
-
-
-def update
-  @user = User.find_by_password_reset_token!(params[:id])
-  if @user.password_reset_sent_at < 2.hours.ago
-    redirect_to new_password_reset_path, :alert => "Password &crarr; 
-      reset has expired."
-  elsif @user.update_attributes(params[:user])
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+    redirect_to new_password_reset_path, :alert => "Password &crarr; reset has expired."
+    elsif @user.update_attributes(params[:user])
     redirect_to root_url, :notice => "Password has been reset."
-  else
+    else
     render :edit
   end
 end
